b077c529261a8e926d6a5c8054555223317e0d5c
Abdrabdra <dico.bb15@mail.ru>
2022-12-19 23:58:46 +0600

    added OneHomeSkeleton

--- src/components/screens/Main/Home/OneHome/OneHome.tsx
+++ src/components/screens/Main/Home/OneHome/OneHome.tsx
@@ -1,4 +1,4 @@
-import { Box, Container, Skeleton, Stack, Typography } from "@mui/material"
+import { Box, Container, Stack } from "@mui/material"
 import { useGetOneAnnouncementQuery } from "@store/rtk-api/announcement-rtk/announcementEndpoints"
 import { useParams } from "react-router-dom"
 
@@ -11,6 +11,7 @@ import {
 } from "./BodyOneHome"
 import OptionBox from "./BodyOneHome/OptionBox"
 import SimilarBox from "./BodyOneHome/SimilarBox"
+import OneHomeSkeleton from "./OneHomeSkeleton"
 
 const OneHome = () => {
 	const { announceId } = useParams()
@@ -23,26 +24,9 @@ const OneHome = () => {
 		<Box>
 			<Container>
 				{isLoading ? (
-					<Stack spacing={1}>
-						<Skeleton
-							animation={"wave"}
-							variant={"rectangular"}
-							width={"100%"}
-							height={200}
-							sx={{
-								borderRadius: "20px"
-							}}
-						/>
-						<Skeleton />
-						<Stack>
-							<Skeleton animation={"wave"} width="50%" sx={{ pt: 1 }} />
-							<Skeleton animation={"wave"} width="35%" sx={{ pt: 1 }} />
-						</Stack>
-						<Skeleton animation={"wave"} height={287} />
-						<Skeleton animation={"wave"} height={137} />
-					</Stack>
+					<OneHomeSkeleton />
 				) : data ? (
-					<Stack spacing={1}>
+					<Stack spacing={2}>
 						<ImageBox />
 						<TagBox />
 						<TitleBox
