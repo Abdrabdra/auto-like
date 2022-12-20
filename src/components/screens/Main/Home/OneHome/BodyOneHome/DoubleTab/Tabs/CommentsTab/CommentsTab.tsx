e78994d7d1dad75fef208de0c28c719ccc0c4866
Abdrabdra <dico.bb15@mail.ru>
2022-12-20 22:33:10 +0600

    added recursive, fixes in announcement commentsTab

--- src/components/screens/Main/Home/OneHome/BodyOneHome/DoubleTab/Tabs/CommentsTab/CommentsTab.tsx
+++ src/components/screens/Main/Home/OneHome/BodyOneHome/DoubleTab/Tabs/CommentsTab/CommentsTab.tsx
@@ -9,10 +9,10 @@ import {
 } from "@mui/material"
 import { useParams } from "react-router-dom"
 
-import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder"
 import { useGetCommentsQuery } from "@store/rtk-api/comments-rtk/commentEndpoints"
 
 import CommentsTabSkeleton from "./CommentsTabSkeleton"
+import CommentsTabOne from "./CommentsTabOne"
 
 interface Props {
 	commentsCount: number
@@ -28,7 +28,7 @@ const CommentsTab: FC<Props> = ({ commentsCount }) => {
 	const { data, isLoading, isSuccess } = useGetCommentsQuery(queryParams)
 
 	return (
-		<Stack spacing={2}>
+		<Stack spacing={4}>
 			{commentsCount === 0 ? (
 				<Stack
 					justifyContent="center"
@@ -43,53 +43,9 @@ const CommentsTab: FC<Props> = ({ commentsCount }) => {
 					Нет Комментарий
 				</Stack>
 			) : isLoading ? (
-				<>
-					<CommentsTabSkeleton />
-				</>
+				<CommentsTabSkeleton />
 			) : isSuccess ? (
-				data.data.map((row) => (
-					<Stack
-						key={row.id}
-						spacing={1}
-						sx={{
-							height: "122px",
-							padding: "16px",
-							borderRadius: "12px",
-							backgroundColor: "common.white"
-						}}
-					>
-						<Stack
-							direction="row"
-							justifyContent={"space-between"}
-							alignContent={"end"}
-						>
-							<Typography
-								alignSelf={"center"}
-								sx={{ fontSize: 16, fontWeight: 600 }}
-							>
-								Пользователь
-								{/* {row.name} */}
-							</Typography>
-							<IconButton color="primary" sx={{ padding: 0 }}>
-								<FavoriteBorderIcon />
-							</IconButton>
-						</Stack>
-						<Typography sx={{ color: "secondary.900" }}>{row.text}</Typography>
-						<Box sx={{ width: "100%", display: "flex", justifyContent: "end" }}>
-							<Button
-								sx={{
-									width: "60px",
-									height: "20px",
-									fontSize: "14px",
-									fontWeight: 500,
-									textDecoration: "underline"
-								}}
-							>
-								Ответить
-							</Button>
-						</Box>
-					</Stack>
-				))
+				data.data.map((row) => <CommentsTabOne key={row.id} row={row} />)
 			) : (
 				<Stack>Error</Stack>
 			)}
--- src/store/rtk-api/comments-rtk/commentEndpoints.ts
+++ src/store/rtk-api/comments-rtk/commentEndpoints.ts
@@ -4,7 +4,10 @@ import { ICommentResponse, ICreateCommentRequest } from "types/Comment/Comment"
 
 export const commentEndpoints = commentApi.injectEndpoints({
 	endpoints: (builder) => ({
-		getComments: builder.query<ICommentResponse, { carId?: string }>({
+		getComments: builder.query<
+			ICommentResponse,
+			{ carId?: string; parentId?: number }
+		>({
 			query: (arg) => {
 				return {
 					url: "/comments",
