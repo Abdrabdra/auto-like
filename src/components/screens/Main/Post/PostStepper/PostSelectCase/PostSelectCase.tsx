4ea04fce7bf35a1577030097e8b3dbad7d3c5c76
Abdrabdra <dico.bb15@mail.ru>
2022-12-27 19:40:59 +0600

    some changes

--- src/components/screens/Main/Post/PostStepper/PostSelectCase/PostSelectCase.tsx
+++ src/components/screens/Main/Post/PostStepper/PostSelectCase/PostSelectCase.tsx
@@ -1,29 +1,8 @@
-import { Button, Stack, Typography } from "@mui/material"
+import { Stack } from "@mui/material"
 
-import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight"
-import { useDispatch } from "react-redux"
-
-import { RootState, useTypedSelector } from "@store/index"
-import {
-	incrementStep,
-	setFormSelectedCase
-} from "@store/reducers/stepper/stepper.slice"
 import CaseList from "./CaseList"
 
 const PostSelectCase = () => {
-	const dispatch = useDispatch()
-	const selectedCase = useTypedSelector(
-		(state: RootState) => state.stepper.form.selectedCase
-	)
-
-	const handleClick = (value: string) => {
-		setTimeout(() => {
-			dispatch(setFormSelectedCase(value))
-			console.log(value)
-			dispatch(incrementStep())
-		}, 250)
-	}
-
 	return (
 		<Stack spacing={1.8}>
 			<CaseList />
