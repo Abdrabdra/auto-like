4c696a60fd2b5b423ed4557ae97a1fcb941319ec
Abdrabdra <dico.bb15@mail.ru>
2023-05-08 22:26:25 +0600

    fix

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-export const DEV_API = "https://avtolike.kz/"
+export const DEV_API = "https://api.avtolike.kz/"
 // export const DEV_API = "http://localhost:3000/"
 // export const PROD_API = "https://";
 
